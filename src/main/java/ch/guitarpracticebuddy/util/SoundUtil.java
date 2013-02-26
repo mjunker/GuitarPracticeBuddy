39750a1cb559170f58fa65eb0cb3c125deae74fb
mjunker <junker.matt@gmail.com>
2013-02-26 16:03:42 +0100

    OutOfMemory fixed: clips are now closed properly.

--- src/main/java/ch/guitarpracticebuddy/util/SoundUtil.java
+++ src/main/java/ch/guitarpracticebuddy/util/SoundUtil.java
@@ -5,8 +5,11 @@ import org.apache.commons.io.IOUtils;
 
 import javax.sound.sampled.*;
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,54 +20,36 @@ import java.io.InputStream;
  */
 public class SoundUtil {
 
+    private static Map<SoundFile, byte[]> soundFileCache = new HashMap<>();
+
     public static void playSound(SoundFile soundFile) {
         try {
-            loadClip(soundFile).start();
+            final Clip clip = AudioSystem.getClip();
+            clip.open(loadClip(soundFile));
+            clip.start();
+            clip.addLineListener(new LineListener() {
+                @Override
+                public void update(LineEvent event) {
+                    if (event.getType() == LineEvent.Type.STOP) {
+                        clip.close();
+                    }
+                }
+            });
+
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
 
-    private static Clip loadClip(SoundFile soundFile) throws LineUnavailableException, UnsupportedAudioFileException, IOException {
-        Clip clip = AudioSystem.getClip();
-        InputStream bufferedIn = new BufferedInputStream(GuitarPracticeBuddyApplication.class.getResourceAsStream(soundFile.getFileName()));
-
+    private static AudioInputStream loadClip(SoundFile soundFile) throws LineUnavailableException, UnsupportedAudioFileException, IOException {
+        if (soundFileCache.get(soundFile) == null) {
+            InputStream fileInputStream = GuitarPracticeBuddyApplication.class.getResourceAsStream(soundFile.getFileName());
+            soundFileCache.put(soundFile, IOUtils.toByteArray(fileInputStream));
+        }
+        BufferedInputStream bufferedIn = new BufferedInputStream(new ByteArrayInputStream(soundFileCache.get(soundFile)));
         AudioInputStream inputStream = AudioSystem.getAudioInputStream(bufferedIn);
-        clip.open(inputStream);
-        return clip;
+        return inputStream;
     }
 
-    public static void play(SoundFile soundFile) {
-        try {
-            tryToPlay(soundFile);
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-    private static void tryToPlay(SoundFile soundFile) throws LineUnavailableException, IOException {
-        final AudioFormat audioFormat = new AudioFormat(44100, 8, 1, true, true);
-        final DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat, 1);
-        final SourceDataLine soundLine;
-
-        soundLine = (SourceDataLine) AudioSystem.getLine(info);
-
-        soundLine.open(audioFormat);
-        soundLine.start();
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(GuitarPracticeBuddyApplication.class.getResourceAsStream(soundFile.getFileName()));
-
-        byte[] bytes = IOUtils.toByteArray(bufferedInputStream);
-        int bufferSize = 2200;
-        byte[] buffer = new byte[bufferSize];
-        for (int i = 0; i < bufferSize; i++) {
-
-            buffer[i] = (byte) (30);
-        }
-        // the next call is blocking until the entire buffer is
-        // sent to the SourceDataLine
-        soundLine.write(bytes, 0, bytes.length);
-
-    }
 }
